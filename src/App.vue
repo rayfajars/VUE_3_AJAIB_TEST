83e22109e6174fc5ddb8f18e7fe3bbcd0d55eaf3
Ray Fajar S <rayfajars.com>
2022-04-24 18:33:27 +0700

    add AXIOS and test get API

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,36 @@
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <Table />
+
+  {{data}}
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import Table from './components/Table.vue'
+import Table from "./components/Table.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Table
-  }
-}
+    Table,
+  },
+  data() {
+    return {
+      data: [],
+    };
+  },
+  created() {
+    axios
+      .get(``)
+      .then((response) => {
+        this.data = response.data.results;
+        console.log(response.data.results);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import axios from 'axios'
+axios.defaults.baseURL = 'https://randomuser.me/api/';
+window.axios = axios;
 
 createApp(App).mount('#app')
